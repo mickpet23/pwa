1e9d5500cbe20703108edcdc44ea92dcfbe3ed0e
batman <mickpet23@yahoo.com>
2019-08-14 09:47:47 -0700

    remove console log

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-console.log('db accessed')
-/*
 import idb from 'idb';
 
 var dbPromise = idb.open('test-db', 1, upgradeDb => {
 	var keyValStore = upgradeDb.createObjectStore('keyval');
 	keyValStore.put('world', 'hello');
 });
-*/
+
