b616a3bc1b75bb9c40b92bdfe150f963e718870c
batman <mickpet23@yahoo.com>
2019-08-14 09:43:07 -0700

    make import the first line

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-console.log('db accessed')
-
 import idb from 'idb';
-
+console.log('db accessed')
 var dbPromise = idb.open('test-db', 1, upgradeDb => {
 	var keyValStore = upgradeDb.createObjectStore('keyval');
 	keyValStore.put('world', 'hello');
