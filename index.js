8cde141ffc1e4bb9124ea3d679343b1621223010
batman <mickpet23@yahoo.com>
2019-08-14 09:45:56 -0700

    comment stuff out

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import idb from 'idb';
 console.log('db accessed')
+/*
+import idb from 'idb';
+
 var dbPromise = idb.open('test-db', 1, upgradeDb => {
 	var keyValStore = upgradeDb.createObjectStore('keyval');
 	keyValStore.put('world', 'hello');
 });
+*/
