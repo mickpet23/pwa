155bfd5a76072abec612e14562d974753df9bf53
batman <mickpet23@yahoo.com>
2019-08-14 09:41:32 -0700

    add index.js test indexedDb

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('db accessed')
+
+import idb from 'idb';
+
+var dbPromise = idb.open('test-db', 1, upgradeDb => {
+	var keyValStore = upgradeDb.createObjectStore('keyval');
+	keyValStore.put('world', 'hello');
+});
