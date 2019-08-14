7ca4ada731f783f23c0a01d44cf7adf4736d39a4
batman <mickpet23@yahoo.com>
2019-08-13 21:48:00 -0700

    open cache

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-console.log('sw.js clocking in');
+//console.log('sw.js clocking in');
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('stativ-v1').then(cache => {
+			return cache.addAll([
+				'/pwa/',
+				'/pwa/index.html'
+			]);
+		})
+	)
+})
+
