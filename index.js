36bbe4e9afcbcc9514b9d1df788854aa72e97d5e
batman <mickpet23@yahoo.com>
2019-08-15 08:47:33 -0700

    change syntax

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import { openDB, deleteDB, wrap, unwrap } from 'https://unpkg.com/idb?module';
 //import idb from 'idb';
 
+var dbPromise = openDB('test-db', 1, {
+	upgrade(dbPromise, oldVersion, newVersion, transaction) {
+		var keyValStore = dbPromise.createObjectStore('keyval')
+		keyValStore.put('world', 'hello')
+	}
+})
 
-var dbPromise = openDB('test-db', 1, upgradeDb => {
-	var keyValStore = upgradeDb.createObjectStore('keyval');
-	keyValStore.put('world', 'hello');
-});
+// var dbPromise = openDB('test-db', 1, upgradeDb => {
+// 	var keyValStore = upgradeDb.createObjectStore('keyval');
+// 	keyValStore.put('world', 'hello');
+// });
+
+
 
