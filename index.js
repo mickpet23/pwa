d3fa835f01ddfac039d2bcc6415958371ddd5311
batman <mickpet23@yahoo.com>
2019-08-15 09:21:25 -0700

    read data

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import { openDB, deleteDB, wrap, unwrap } from 'https://unpkg.com/idb?module';
 //import idb from 'idb';
 
-var dbPromise = openDB('test-db', 1, {
+let dbPromise = openDB('test-db', 1, {
 	upgrade(dbPromise, oldVersion, newVersion, transaction) {
-		var keyValStore = dbPromise.createObjectStore('keyval')
+		let keyValStore = dbPromise.createObjectStore('keyval')
 		keyValStore.put('world', 'hello')
 	}
+})
+
+dbPromise.then(db => {
+	let tx = db.transaction('keyval');
+	let keyValStore = tx.objectStore('keyval');
+	return keyValStore.get('hello');
+}).then(val => {
+	console.log('The value of "hello" is ', val);
 })
 
 // var dbPromise = openDB('test-db', 1, upgradeDb => {
